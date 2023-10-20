ddf2fa3b9a6245ee0484831f1f0fc57d34b1c9b4
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2023-10-20 15:42:36 +0200

    Added basic pipe functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{stdin, stdout, Write};
-use std::process::Command;
+use std::process::{Command, Child, Stdio};
 use std::path::Path;
 use std::env;
 
@@ -13,36 +13,72 @@ fn main() {
 		let mut input = String::new(); // var to store user input
 		stdin().read_line(&mut input).unwrap(); // reads user input, and writes into input string
 
-		// everything after the first whitespace character
-		// is interpreted as args to the command
-		let mut parts = input.trim().split_whitespace();
-		let command = parts.next().unwrap();
-		let args = parts;
+    // must be peekable so we know when we are on the last command
+    let mut commands = input.trim().split(" | ").peekable();
+    let mut previous_command = None;
 
-    match command {
+    while let Some(command) = commands.next() {
 
-      "cd" => {
-        let new_dir = args.peekable().peek().map_or("/", |x| *x);
-        let root = Path::new(new_dir);
-        if let Err(e) = env::set_current_dir(&root) {
-          eprintln!("{}", e);
+  		// everything after the first whitespace character
+  		// is interpreted as args to the command
+  		let mut parts = command.trim().split_whitespace();
+  		let command = parts.next().unwrap();
+  		let args = parts;
+
+      match command {
+  
+        "cd" => {
+          let new_dir = args.peekable().peek().map_or("/", |x| *x);
+          let root = Path::new(new_dir);
+          if let Err(e) = env::set_current_dir(&root) {
+            eprintln!("{}", e);
+          }
+
+          previous_command = None;
+
+        },
+  
+        "exit" => return,
+  
+        command => {
+          let stdin = previous_command
+            .map_or(
+              Stdio::inherit(),
+              |output: Child| Stdio::from(output.stdout.unwrap())
+            );
+
+          let stdout = if commands.peek().is_some() {
+            // if there is another command piped behind this one
+            // prepare to send output to the next command
+            Stdio::piped()
+          } else {
+            // otherwise, if there are no more commands piped behind this one
+            // send output to shell stdout
+            Stdio::inherit()
+          };
+
+          let output = Command::new(command)
+            .args(args)
+            .stdin(stdin)
+            .stdout(stdout)
+            .spawn();
+  
+          // handle malformed user input
+          match output {
+            Ok(mut output) => { previous_command =  Some(output); },
+            Err(e) => {
+              previous_command = None;
+              eprintln!("{}", e);
+            },
+          };
         }
-      },
-
-      "exit" => return,
-
-      command => {
-        let child = Command::new(command)
-          .args(args)
-          .spawn();
-
-          // gracefully handle malformed user input
-
-        match child {
-          Ok(mut child) => { child.wait(); },
-          Err(e) => eprintln!("{}", e),
-        };
       }
     }
+
+    if let Some(mut final_command) = previous_command {
+      // block until the final command has finished
+      final_command.wait();
+    }
+
   }
 }
