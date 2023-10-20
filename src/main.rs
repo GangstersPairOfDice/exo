8c06a6c82ff1e9594f254c7bf876ece24cedab33
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2023-10-20 14:05:06 +0200

    Added basic error handling, thus fixing crash on unknown command, and added cd and exit commands. This commit was done by using exo itself!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{stdin, stdout, Write};
 use std::process::Command;
+use std::path::Path;
+use std::env;
 
 fn main() {
 	loop{
@@ -16,13 +18,31 @@ fn main() {
 		let mut parts = input.trim().split_whitespace();
 		let command = parts.next().unwrap();
 		let args = parts;
-	
-		let mut child = Command::new(command)
-			.args(args)
-			.spawn()
-			.unwrap();
 
-		// don't accept another command until this one completes
-		child.wait();
-	}
+    match command {
+
+      "cd" => {
+        let new_dir = args.peekable().peek().map_or("/", |x| *x);
+        let root = Path::new(new_dir);
+        if let Err(e) = env::set_current_dir(&root) {
+          eprintln!("{}", e);
+        }
+      },
+
+      "exit" => return,
+
+      command => {
+        let child = Command::new(command)
+          .args(args)
+          .spawn();
+
+          // gracefully handle malformed user input
+
+        match child {
+          Ok(mut child) => { child.wait(); },
+          Err(e) => eprintln!("{}", e),
+        };
+      }
+    }
+  }
 }
